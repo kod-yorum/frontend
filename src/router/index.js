c9f39d7d22e724b3c9ab025ad2524df7f77655d0
Ahmet Rıdvan Çağlar <arcaglar@Ahmet-MacBook-Pro.local>
2019-09-17 11:26:56 +0300

    init commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/components/pages/Home'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
+
+export default router
