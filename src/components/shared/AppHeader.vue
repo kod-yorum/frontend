c89039f2b2b47ec6ea038e0ffcdce6bf5d06c7a0
Ahmet Rıdvan Çağlar <arcaglar@Ahmet-MacBook-Pro.local>
2019-11-12 18:20:18 +0300

    header login & signup navigate button

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -2,6 +2,7 @@
     <nav class="navbar">
       <div class="container-fluid">
         <a href="#" class="logo">KODYORUM</a>
+        <a href="" class="login">Giriş Yap / Kayıt ol</a>
       </div>
     </nav>
 </template>
@@ -29,5 +30,20 @@
   align-items: center;
   transition: .2s ease-out;
   letter-spacing: .1em;
+}
+.login {
+  height: 50px;
+  float: right;
+  padding: 0px 12px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #6f6f6f;
+  line-height: 50px;
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    color: #000;
+    border-bottom: 2px solid #2164f3;
+  }
 }
 </style>
